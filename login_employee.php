5f8bec101a9e2c06b8b2e5abd7a3210bb5687435
Shrishti Wahal <shrishticharan7@gmail.com>
2015-07-01 15:17:36 +0530

    fmskdfmksm
    czcjn cm

--- login_employee.php
+++ login_employee.php
@@ -1,27 +1,47 @@
 <?php
-$dbc=mysqli_connect('localhost','root','','spark') or die('Database Connection error');
-$email1=$_POST['email'];
-$password1=crypt($_POST['password']);
-if(isset($email1) && isset($password1))
-{
-session_start();
+// Start the session
+  session_start();
+ require_once('connect.php');
+  // Clear the error message
+  $error_msg = "";
+//echo "g";
+if (!isset($_SESSION['ID'])) {
+ //echo "ok";
 
+$dbc = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD) or die(mysql_error());
+mysql_select_db(DB_NAME);
+echo "test";
+$email1=mysql_real_escape_string(trim($_POST['email']),$dbc);
+$password1 = mysql_real_escape_string(trim($_POST['password']),$dbc);
+$pass1=md5($password1);
 
-$_SESSION['login_user']=$email1;
-$query="SELECT * from register_emp where email='$email1' and password='$password1' ";
-$result=mysqli_query($dbc,$query) or die('Error in querying');
-if(mysqli_num_rows($result)!=1)
+if(isset($email1) && isset($pass1))
 {
-echo 'Username and password is wrong';
+//echo "m";
+$query="SELECT * from register_emp where email='$email1' and password='$pass1'";
+$result=mysql_query($query,$dbc);
+//echo "po";
+if(mysql_num_rows($result)==1 )
+{
+$row=mysql_fetch_array($result);
+$_SESSION['ID'] = $row['ID'];
+$_SESSION['email']=$row['email']; 
+setcookie('user_id', $row['ID'], time() + (60 * 60 * 24 * 30));    // expires in 30 days
+setcookie('email', $row['email'], time() + (60 * 60 * 24 * 30));  // expires in 30 days
+header('location: finaluserprofile.php');
+}
+else
+{
+echo 'Username or password is wrong';
+}
+}
+
+mysql_close();
 
 }
 else
 {
-echo 'Successful Login';
-$_SESSION['login_user']=$email1; 
-header("location: profile.php");
+echo"got it,already logged in";
+header('location: viewprofile.php');
 }
-}
-mysqli_close($dbc);
-
 ?>
